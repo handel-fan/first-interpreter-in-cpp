3510c1a69e6377a97f96a7b75bcc2837a1cfd4b1
handel-fan <jegan.sivakasiulaganathan@gmail.com>
2025-05-15 18:24:21 -0400

    porting tokens over to a new type to be enumerable. Some other updates. Preparing to remove all non-personal code.

--- monkey02/include/token/types.h
+++ monkey02/include/token/types.h
@@ -3,5 +3,46 @@
 #include <string>
 
 // types.h
-using TokenType = std::string;
+// using TokenType = std::string;
+
+enum class TokenType {
+
+  ILLEGAL,
+  END_OF_FILE,
+
+  // Identifiers + literals
+  IDENT,
+  INT,
+
+  // Operators
+  ASSIGN,
+  PLUS,
+  MINUS,
+  BANG,
+  ASTERISK,
+  SLASH,
+  LT,
+  GT,
+  EQ,
+  NOT_EQ,
+
+  // Delimiters
+  COMMA,
+  SEMICOLON,
+  LPAREN,
+  RPAREN,
+  LBRACE,
+  RBRACE,
+
+  // Keywords
+  FUNCTION,
+  LET,
+  TRUE,
+  FALSE,
+  IF,
+  ELSE,
+  RETURN,
+}; // namespace token
+
+;
 #endif // TYPES_H
